26941421d67d0f949c19b1543e69663ee35e1f09
bpifko <61442708+bpifko@users.noreply.github.com>
2020-03-11 14:12:51 -0400

    Update ben.py

--- ben.py
+++ ben.py
@@ -72,7 +72,7 @@ def run_bot(r, comment_replied_to):
 def run_bot(r, comment_replied_to):
     for comment in r.subreddit('lotrmemes').comments(limit=25):
         for keyword in keywords:
-            if "!Aragorn-bot" in comment.body and comment.id not in comment_replied_to and not comment.author == r.user.me:
+            if "second breakfast" in comment.body and comment.id not in comment_replied_to and not comment.author == r.user.me:
                 print( "Breakfast has been found")
                 comment.reply("You've already had it")
                 comment_replied_to.append(comment.id)
